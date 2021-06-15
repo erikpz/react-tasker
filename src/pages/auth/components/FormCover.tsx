76e48bb9d862b3ff465e8b265f346eca7c5deb5c
Erik Pizana <erikpzcanedo@outlook.com>
2021-06-14 20:57:48 -0500

    Dependencies

--- src/pages/auth/components/FormCover.tsx
+++ src/pages/auth/components/FormCover.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid, makeStyles, Theme, Typography } from "@material-ui/core";
+import { Box, makeStyles, Theme, Typography } from "@material-ui/core";
 import { useLocation } from "react-router";
 
 export const FormCover = () => {
